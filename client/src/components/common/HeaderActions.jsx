fd0cdae3ce399616e7f233411a9b4f296b8d5f99
AihamJassar <165441373+AihamJassar@users.noreply.github.com>
2026-09-17 17:21:59 +0300

    refactor: update import statement for Link and remove unused code in HeaderActions

--- client/src/components/common/HeaderActions.jsx
+++ client/src/components/common/HeaderActions.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 import { Search, ShoppingCartIcon, User, UserCircle, LogOut } from "lucide-react";
 import { logout } from "../../store/thunks/authThunk";
@@ -11,26 +11,9 @@ export const HeaderActions = ({ dispatch: modalDispatch }) => {
 
   return (
     <div className="flex items-center gap-5">
-      {/* <div className="flex items-center gap-2.5  p-1 sm:py-1.5 sm:px-2.5 bg-white sm:bg-gray-100 rounded-2xl hover:bg-gray-200">
-        <Search
-          aria-label="Search bar"
-          className="cursor-pointer sm:cursor-auto"
-          onClick={() => modalDispatch({ type: "OPEN_SEARCHBAR" })}
-        />
-        <input
-          type="text"
-          id="search"
-          name="search"
-          className={`outline-none hidden sm:block sm:w-40 xl:w-80`}
-          placeholder="Search for products..."
-        />
-      </div> */}
       <Link
         to={'/cart'}
         className="relative"
-        onClick={() => {
-          !isAuthenticate && modalDispatch({ type: "OPEN_SIGNIN_MODAL" });
-        }}
       >
         <ShoppingCartIcon
           className="cursor-pointer transition-colors duration-300 rounded-md hover:bg-gray-200"
@@ -41,21 +24,6 @@ export const HeaderActions = ({ dispatch: modalDispatch }) => {
         </span>
       </Link>
       {isAuthenticate ? (
-        // user.role === "admin" ? (
-        //   <Link
-        //     to={"/dashboard"}
-        //     className="flex justify-center items-center gap-1.5 w-28 h-12 rounded-xl shadow-md bg-black text-white cursor-pointer"
-        //   >
-        //     Dashboard
-        //   </Link>
-        // ) : (
-        //   <Link
-        //     to={"/profile"}
-        //     className="flex justify-center items-center w-fit hover:bg-gray-200 rounded-full cursor-pointer"
-        //   >
-        //     <UserCircle size={35} />
-        //   </Link>
-        // )
         <button
           className="flex justify-center items-center gap-1.5 w-28 h-12 rounded-xl shadow-md bg-black text-white cursor-pointer"
           disabled={status.logout === "loading"}
